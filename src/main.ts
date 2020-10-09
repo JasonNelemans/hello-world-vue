441611d0e5dbbfbe1bccfc0012520fc481eaa730
JasonNelemans <jasonnelemans@hotmail.com>
2020-10-09 14:36:12 +0200

    use BootstrapVue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import router from './router';
+import store from './store';
+import BootstrapVue from 'bootstrap-vue';
 
+import App from './App.vue';
+
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app');
