4cf762b13a914415a2d8dd3af3227ff8f3ea46f7
JasonNelemans <jasonnelemans@hotmail.com>
2020-10-16 11:04:53 +0200

    applied minor styling and imported bslistgroup

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,10 +6,16 @@
       </template>
 
       <hr class="my-4" />
-
-      <p v-for="(answer, index) in answers" :key="index">
-        {{ answer }}
-      </p>
+      
+      <b-list-group>
+        <b-list-group-item 
+          v-for="(answer, index) in answers" 
+          :key="index"
+        >
+          {{ answer }}
+        </b-list-group-item>
+      </b-list-group>
+      
 
       <b-button variant="primary" href="#">Submit</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
@@ -35,3 +41,13 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+  .btn {
+    margin: 0 5px;
+  }
+</style>
